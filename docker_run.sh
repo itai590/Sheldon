f2de512d577d1f8e4adf89928d124f0af2604867
Itai <itai590@gmail.com>
2024-01-07 19:55:54 +0200

    Update docker run command to include volume mapping for config.json

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 repo_name="hey-sheldon"
-docker run -d --device /dev/snd:/dev/snd --name hey-sheldon-backend $repo_name:latest
+docker run -d --device /dev/snd:/dev/snd --volume /home/pi/Sheldon/config.json:/app/config.json --name hey-sheldon-backend $repo_name:latest
