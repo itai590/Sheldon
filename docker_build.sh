89f9d536082136f12fe25db7482f7eb78b04fd8b
Itai <itai590@gmail.com>
2024-01-07 16:58:43 +0200

    Increment build_id in docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -2,5 +2,6 @@
 repo_name="hey-sheldon"
 
 build_id=$(docker inspect $repo_name | jq -r '.[0].RepoTags[0]' | awk -F':' '{print $2}')
+build_id=$((build_id+1))
 docker build -t $repo_name:latest .
 docker tag $repo_name:latest "$repo_name:$build_id"
