f773abb85736e9b5bd1cadc54278e43616e95326
Itai <itai590@gmail.com>
2024-01-09 13:40:40 +0200

    Remove unnecessary code and update Docker command

--- RUN.sh
+++ RUN.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-repo_name="hey-sheldon"
-
 # Pre Run
 docker stop hey-sheldon-backend || true
 docker rm hey-sheldon-backend || true
 
 # Run
-docker run -d -e TZ=Asia/Jerusalem --device /dev/snd:/dev/snd --volume /home/pi/Sheldon/config.json:/app/config.json --name hey-sheldon-backend $repo_name:latest
-# docker run -d -e TZ=Asia/Jerusalem --device /dev/snd:/dev/snd --volume /home/pi/Sheldon/config.json:/app/config.json --name hey-sheldon-backend-4 hey-sheldon:latest
-
-
 docker compose up -d
